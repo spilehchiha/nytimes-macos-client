9c08e5e94ec27e83b7571de676e139a0d2b9c73f
Sina Pilehchiha <sina.pilehchiha@mail.concordia.ca>
2019-06-15 16:43:47 -0400

    Initial Commit

---
+++ nytimes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  nytimes
+//
+//  Created by Sina Pilehchiha on 2019-06-15.
+//  Copyright © 2019 Concordia University. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
